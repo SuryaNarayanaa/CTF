f65818dea7e674cb94228f2f35d380e5cd169929
Surya Narayanaa N T <thangarajsurya001@gmail.com>
2024-10-02 00:34:58 +0530

    updates in routers

--- routers/ctfRoutes.js
+++ routers/ctfRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const ctfController = require('../controllers/ctfController'); 
+
+
 router.get('/questions', ctfController.getQuestions);
 router.post('/submit', ctfController.submitAnswer);
 
--- routers/homeRoutes.js
+++ routers/homeRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('../controllers/homeController'); // Import the controller
+const homeController = require('../controllers/homeController'); 
 
-// Define the home route
+
 router.get('/', homeController.home);
 
 module.exports = router;
