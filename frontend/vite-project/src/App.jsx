3fd28382d59009910f8629f04c10dce3e309df4b
Surya Narayanaa N T <thangarajsurya001@gmail.com>
2024-10-30 22:25:09 +0530

    Add GSAP library and implement LeaderboardPage component with layout adjustments

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AdminPage from './pages/AdminPage';
-import LeaderboardPage from './pages/LeaderboardPage ';
+import LeaderboardPage from './pages/LeaderboardPage';
 import QuestionPage from './pages/QuestionPage';
 import './index.css';
 const App = () => {
   return (
+    <>        
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/admin/*" element={<AdminPage />} />
         <Route path="/leaderboard" element={<LeaderboardPage />} />
-        <Route path="/question" element={<QuestionPage />} />
+        <Route path="/challenges" element={<QuestionPage />} />
+
 
       </Routes>
+      </>
   );
 };
 
