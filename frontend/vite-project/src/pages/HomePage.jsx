5ce2006898ff3ae2ef804c19a0c25a23a498177f
Surya Narayanaa N T <thangarajsurya001@gmail.com>
2024-10-30 12:01:58 +0530

    Add custom button styles and integrate Google font in HomePage

--- frontend/vite-project/src/pages/HomePage.jsx
+++ frontend/vite-project/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { BackgroundBeams } from '../components/ui/background-beams';
-import { Boxes } from '../components/ui/background-boxes';  // Ensure the correct import path if different
+import { Boxes } from '../components/ui/background-boxes';
+import './HomePage.css';
 import { 
   Shield, 
   Trophy, 
@@ -12,30 +13,58 @@ import {
 
 const HomePage = () => 
 {
-  // Background Beams Image
   useEffect(() => {
     document.body.style.backgroundImage = "url('bg.png')";
     document.body.style.backgroundSize = 'cover';
     document.body.style.backgroundRepeat = 'no-repeat';
     document.body.style.backgroundAttachment = 'fixed';
-
-    
-
-    return () => {
-      document.body.style.backgroundImage = '';
-    };
   }, []);
 
-
   return (
-    <div className="flex items-center justify-center min-h-screen relative">
-     
-      <div className="relative z-0">
-        <Boxes />
-      </div>
-
-    </div>
-  );
+      <>
+        <div className="">
+          <img 
+            src="LOGO.gif"
+            alt="Flag Logo"
+            className="absolute"
+            style={{
+              top: '31%', 
+              left: '50%',
+              transform: 'translate(-50%, -35%)', 
+              height: '37%', 
+              width: 'auto', 
+              maxHeight: '100%', 
+              maxWidth: '100%', 
+            }}
+          />
+        </div>
+        
+        <div className="flex items-center justify-center min-h-screen ">
+          <div className="absolute z-1/2">
+            <Boxes />
+          </div>
+        </div>
+    
+        {/* Button Container */}
+        <div className="absolute transform -translate-x-1/2 flex space-x-4 z-10"
+        style={{
+          top: '58%', 
+          left: '50%',
+          transform: 'translate(-50%, -35%)', 
+          maxHeight: '100%', 
+          maxWidth: '100%', 
+        }}>
+        <div className="cta-buttons">
+        <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-black text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400 font-['Press_Start_2P']">
+        Log Out</button>
+        <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-black text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400 font-['Press_Start_2P']">
+        → Scoreboard</button>
+          </div>
+        </div>
+      </>
+    );
+  
+    
 };
 
 
