a343a1a51b5861d1b16aba1011f0a70a0968e68c
Pravith1 <pravith1489181@gmail.com>
2024-11-01 18:02:12 +0530

    viteproxy

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -27,6 +27,10 @@ export default defineConfig({
         target: 'http://localhost:3000', 
         changeOrigin: true,
       },
+      '/Admin/questions/questionsByCategory': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
     },
   },
 })
