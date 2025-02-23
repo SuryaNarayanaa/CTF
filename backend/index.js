369758a638ed25baec911665f3497986e2546fa0
Pravith1 <pravith1489181@gmail.com>
2025-02-23 15:50:59 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const connectDB = require('./db')
 require('dotenv').config()
 
 connectDB().then(()=>{
-  app.listen(process.env.PORT||7000,(err)=>{
+  app.listen(process.env.PORT||3000,(err)=>{
     if(!err) console.log(`Server is running in http:localhost:${process.env.PORT || 7000}`)
     else console.log("Failed to start the server")
   })
