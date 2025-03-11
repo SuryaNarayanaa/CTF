0097214ac3ff067465a680814185f20bc80ed32b
Pravith1 <pravith1489181@gmail.com>
2025-03-11 18:47:41 +0530

    transaction

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 connectDB().then(()=>{
   server.listen(process.env.PORT||3000,(err)=>{
-    if(!err) console.log(`Server is running in http:localhost:${process.env.PORT || 7000}`)
+    if(!err) console.log(`Server is running in http:localhost:${process.env.PORT || 3000}`)
     else console.log("Failed to start the server")
   })
 })
