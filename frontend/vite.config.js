ae3aa992b42d366ba56ee96f1547681ab7e5be01
Surya Narayanaa N T <thangarajsurya001@gmail.com>
2025-03-11 22:02:21 +0530

    del package-lock.json mod vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,11 @@
 /* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), // Add Tailwind
-    autoprefixer(), // Add Autoprefixer
+  plugins: [react()
   ],
   resolve: {
       alias: {
@@ -16,6 +13,8 @@ export default defineConfig({
       }
     },
     server: {
+      host: true, // Allow external access
+      allowedHosts: ['ctf-frontend-latest.onrender.com', 'https://hidden-x.vercel.app/'], 
       proxy: {
         '/back': {
           target: 'https://hidden-x-backend.onrender.com/api',
