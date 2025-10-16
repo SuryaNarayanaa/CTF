c1d43fca8970d8a203f6d4b2087ed6f45fbe14de
Surya Narayanaa N T <thangarajsurya001@gmail.com>
2025-10-16 22:07:14 +0530

    refactor: update vercel and vite configurations for improved routing and host settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,13 +16,6 @@ export default defineConfig({
       host: '0.0.0.0',  // Ensure it binds to all interfaces
       port: process.env.PORT || 5173,  // Use Render’s PORT variable
 
-      allowedHosts: ['ctf-frontend-latest.onrender.com', 'https://hidden-x.vercel.app/'], 
-      proxy: {
-        '/back': {
-          target: 'https://hidden-x-backend.onrender.com/api',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/back/, ''),
-        },
-      },
+      allowedHosts: ['ctf-frontend-latest.onrender.com', 'https://hidden-x.vercel.app/'],
     },
 })
